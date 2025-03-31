81e2c2824643497a3e7f7c0893549f8e546026ac
OmamaAlsunni <omama.alsunni@gmail.com>
2025-03-31 17:53:43 +0100

    Update chem_calc.py

--- chem_calc.py
+++ chem_calc.py
@@ -1081,7 +1081,7 @@ if __name__ == "__main__":
         with col2:
           anhydrous_salt_mass= st.number_input("anhydrous salt mass:", placeholder="10.0", value=None, min_value=0.0,step=0.1)
 
-        at.write("‎ ")
+        st.write("‎‎ ")
         st.write("Elements the hydrated salt consists of:")
         col1, col2 = st.columns(2)
 
